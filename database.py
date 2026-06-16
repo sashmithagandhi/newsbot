b9e20632d2ddc9ea0c61c33e90027ecb974ce191
sashmithagandhi <sashmithagandhi6@gmail.com>
2026-06-16 14:05:50 +0530

    Add requirements.txt and database changes

--- database.py
+++ database.py
@@ -10,7 +10,8 @@ MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_HOST = os.getenv("MYSQL_HOST")
 MYSQL_DB = os.getenv("MYSQL_DB")
 
-DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
+DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
